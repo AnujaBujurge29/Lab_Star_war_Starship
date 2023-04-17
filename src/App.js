d0dc2b54435361360a55c027966a7edfca04253b
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2023-04-17 14:35:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import StarWarAPI from './services/StarWarAPI';
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App js File</h1>
+      <StarWarAPI></StarWarAPI>
     </div>
   );
 }
---
+++ src/Components/StarshipCard.js
@@ -0,0 +1,5 @@
+export default function StarshipCard(StarShipNames){
+    return(
+        <div><p>this is Starship Card</p></div>
+    )
+}
